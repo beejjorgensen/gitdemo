c8394cb100d39c4b589f01fe1c0b76cae00c868c
Brian "Beej Jorgensen" Hall <beej@beej.us>
2023-01-21 19:42:41 -0800

    added

---
+++ hello.py
@@ -0,0 +1,11 @@
+# This is my hello, world project
+
+def main():
+    print("Getting ready to output the message.")
+    print("Finished getting ready.")
+    print("Hello, world!")
+    print("Completed message printing")
+    print("Returning from main")
+
+if __name__ == "__main__":
+    main()
